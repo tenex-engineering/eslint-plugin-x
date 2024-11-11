e16a36916d4236f6b6cb1aba3d00a44980f0a8d3
cnr <cnr@tenex.engineering>
2024-11-11 09:00:23 +0700

    refactor

--- eslint.config.js
+++ eslint.config.js
@@ -17,7 +17,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 const gitignorePath = path.resolve(__dirname, '.gitignore')
 
-/** @type {import('@typescript-eslint/utils').TSESLint.FlatConfig.ConfigArray} */
+/** @type {import('eslint').Linter.Config[]} */
 export default [
   includeIgnoreFile(gitignorePath),
 
